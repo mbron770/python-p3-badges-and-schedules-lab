ac6797165233a483f4a3ced677a8b1ff6bd45c78
mbron770 <mbronfin.tsd@gmail.com>
2023-07-04 19:55:08 -0400

    complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,12 @@
 def badge_maker(name):
-    return None
+    return f"Hello, my name is {name}."
 
 def batch_badge_creator(names):
-    return None
+    return [f"Hello, my name is {name}." for name in names ]
 
 def assign_rooms(names):
-    return None
+    return [f"Hello, {name}! You'll be assigned to room {room}!" for room, name in enumerate(names, start =1)]
+
 
 def printer(names):
-    return None
+   return [print(badge) for badge in batch_badge_creator(names)] + [print(rooms) for rooms in assign_rooms(names)] 
